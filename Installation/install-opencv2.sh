c374c22ecda0b70f866554758c0f042ab8f0f76e
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2020-03-14 22:41:31 +0600

    Add files via upload

---
+++ Installation/install-opencv2.sh
@@ -0,0 +1,11 @@
+cd OpenCV
+cd build
+cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON -DWITH_XINE=ON -DBUILD_EXAMPLES=ON -DENABLE_PRECOMPILED_HEADERS=OFF ..
+make -j2
+sudo make install
+sudo ldconfig
+
+
+# 4. EXECUTE SOME OPENCV EXAMPLES AND COMPILE A DEMONSTRATION
+
+# To complete this step, please visit 'http://milq.github.io/install-opencv-ubuntu-debian'.
