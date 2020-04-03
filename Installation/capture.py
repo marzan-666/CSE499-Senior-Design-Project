35e480d347dbca66a01a6b832b431e8bb2ca37b1
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2020-04-03 06:46:39 +0600

    Add files via upload

---
+++ Installation/capture.py
@@ -0,0 +1,10 @@
+from picamera import PiCamera
+from time import sleep
+
+camera=PiCamera()
+camera.resolution = (500,500)
+
+camera.start_preview()
+sleep(5)
+camera.capture('image.jpg')
+camera.stop_preview()
