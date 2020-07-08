bf733c9d1356b43741b7f476c875163352b15dd0
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2020-07-08 10:20:05 +0600

    Add files via upload

---
+++ push.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+def button_callback(channel):
+    print("Button was pushed!")
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+while True: # Run forever
+    if GPIO.input(8) == GPIO.HIGH:
+        print("Button was pushed!")
+        break
